d11765a769995da8140ae42731221e568a7666d4
Vladimir Jimenez <allejo@me.com>
2026-08-23 20:48:59 +0000

    chore: formatting

--- templates.go
+++ templates.go
@@ -6,5 +6,6 @@ import "embed"
 // tfdocs-extras CLI and the tfdocs-extras-plugin. The template breaks the
 // output into reusable sections (inputs, outputs, objects, reference_links)
 // that can be individually rendered and composed as needed.
+//
 //go:embed templates/structured.tmpl
 var TemplateFS embed.FS
